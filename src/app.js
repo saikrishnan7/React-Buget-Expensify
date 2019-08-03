d431ff885d5ec3432ef15b762934e32b51d39753
Saikrishnan Srivatsan <Sai_Srivatsan@dell.com>
2019-08-03 18:59:23 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/styles.scss'
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(<p>This is my boiler plate code!!</p>, document.getElementById('app'));
