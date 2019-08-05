bfe55566b923caac5893c3d888b6e62da154d6f1
Saikrishnan Srivatsan <Sai_Srivatsan@dell.com>
2019-08-05 07:42:10 -0500

    End of lecture 86 - Setting up actions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import AppRouter from './routers/AppRouter'
-import AppRouter from './routers-portfolio/AppRouter'
+import AppRouter from './routers/AppRouter'
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
---
+++ src/playground/redux-101.js
@@ -0,0 +1,47 @@
+import { createStore } from 'redux';
+
+const store = createStore((state = {count: 0}, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            }
+        case 'RESET': 
+            return {
+                count: 0
+            }
+        default:
+            return state;
+    }
+});
+    
+
+console.log('running');
+
+//Actions gets send to the store rather than object.
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+})
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+
+
+console.log(store.getState());
