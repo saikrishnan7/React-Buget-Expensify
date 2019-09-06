c4fe84bea4dc6458748a2d1f6e94cb1620320b5f
Saikrishnan Srivatsan <Sai_Srivatsan@dell.com>
2019-09-06 11:37:49 -0500

    Added numeral library to application

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
-export const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
-    <div>
+export const ExpenseListItem = ({ id, description, amount, createdAt }) => {
+    return (<div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
-    </div>
-);
+        <p>
+            {numeral(amount / 100).format('$0,0.00')}
+            -
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
+    </div>);
+};
 
 export default ExpenseListItem;
